301a86b50b4058862d88fd15a6eb7e9aaa039204
RoosaJuliaV <86317676+RoosaJuliaV@users.noreply.github.com>
2021-10-29 17:06:55 +0200

    Cleaning up code

--- src/components/SleepRow.js
+++ src/components/SleepRow.js
@@ -1,53 +1,3 @@
-/* import React from "react";
-import { MDBCol, MDBContainer, MDBRow, MDBFooter, MDBCard, MDBIcon } from "mdbreact";
-import { Button, Nav } from "react-bootstrap"
-
-const SleepRow = () => {
-  return (
-      <div className="sleepcontainer">
-              <MDBContainer className="sleepcards">
-            <MDBRow>
-            <div className="d-flex flex-row">
-            <Nav className="navbar fixed-top navbar" id="sleepnav">
-            <div className="container-fluid" id="sleepnavdiv">
-            <h1 className="sleepheading mb-4 ml-0"><strong>Sleep</strong></h1>
-            <Button
-          type="button"
-          className="btn btn-outline-light btn-rounded"
-          data-mdb-ripple-color="dark"
-          id="favoritesbutton"
-          >
-        See all<MDBIcon icon="angle-down" className="ml-2"/>
-        </Button>
-        </div>
-        </Nav>
-        </div>
-        </MDBRow>
-                 <MDBRow>
-                  <MDBCard className="topsleepcard mb-5 ml-4">
-  <div className="text-white d-flex align-items-center py-5 px-4">
-    <div id="sleepcardtitletop">
-      <h3 className="card-title px-3">Listen to the clip of the day now<MDBIcon icon="angle-right" className="ml-4"/></h3>
-    </div>
-  </div>
-</MDBCard>
-</MDBRow>
-<MDBRow className="mx-0">
-<MDBCard className="sleepcard2 px-0">
-  <div className="text-white d-flex py-4 px-1">
-    <div className="rgba-black-light mt-auto pr-4 pr-1" id="sleepcardtitle">
-      <h5 className="card-title">This is the card title</h5>
-    </div>
-  </div>
-</MDBCard>
-</MDBRow>                 
- </MDBContainer>           
-      </div>
-  )
-}
-
-export default SleepRow */
-
 import React from "react";
 import {
   MDBCol,
@@ -116,15 +66,6 @@ const SleepRow = () => {
         </MDBRow>
         <MDBRow>
             <TopCardSleep/>
-         {/* <MDBCard className="topsleepcard mb-5 ml-4">
-            <div className="text-white d-flex align-items-center">
-              <div id="sleepcardtitletop">
-                <h3 className="card-title px-3 ml-4" id="topcardhead">
-                  Listen to the track of the day now
-                </h3>
-              </div>
-            </div>
-  </MDBCard> */}
         </MDBRow>
         <MDBRow className="mx-0">
         {
