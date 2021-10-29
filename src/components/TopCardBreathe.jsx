0caf2ae4e2d5f8252deaa2e459690c08a5acd5af
RoosaJuliaV <86317676+RoosaJuliaV@users.noreply.github.com>
2021-10-29 17:05:33 +0200

    Cleaning up code

--- src/components/TopCardBreathe.jsx
+++ src/components/TopCardBreathe.jsx
@@ -1,4 +1,11 @@
-import { MDBCol, MDBContainer, MDBRow, MDBFooter, MDBCard, MDBIcon } from "mdbreact";
+import {
+  MDBCol,
+  MDBContainer,
+  MDBRow,
+  MDBFooter,
+  MDBCard,
+  MDBIcon,
+} from "mdbreact";
 import { Button, Nav, Card, CardImg, Spinner } from "react-bootstrap";
 import React, { useEffect, useState } from "react";
 import { useParams, Link, withRouter } from "react-router-dom";
@@ -6,107 +13,100 @@ import { connect } from "react-redux";
 import { addSongToPlaylist, playSong } from "../actions";
 import PlayMod from "./PlayMod";
 
-
 const mapStateToProps = (state) => ({
-    currentSong: state.play.currentSong,
+  currentSong: state.play.currentSong,
   playList: state.playList.tracks,
 });
 
 const mapDispatchToProps = (dispatch) => ({
-    addToCurrentSong: (song) => dispatch(playSong(song)),
+  addToCurrentSong: (song) => dispatch(playSong(song)),
   addToPlaylist: (song) => dispatch(addSongToPlaylist(song)),
 });
 
 const TopCardBreathe = ({ addToPlaylist, addToCurrentSong }) => {
+  const [trackArray, setTrackArray] = useState([]);
+  const [albumName, setAlbumName] = useState("");
+  const [albumImage, setAlbumImage] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const params = useParams();
+  const albumId = params.id;
 
-    const [trackArray, setTrackArray] = useState([]);
-    const [albumName, setAlbumName] = useState("");
-    const [albumImage, setAlbumImage] = useState("");
-    const [isLoading, setIsLoading] = useState(true)
-    const params = useParams();
-    const albumId = params.id;
+  const [open, setOpen] = React.useState(false);
 
-     const [open, setOpen] = React.useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
 
-    const handleOpen = () => { 
-      setOpen(true);
-    };
-  
-    const handleClose = () => {
-      setOpen(false);
-    };
-  
-    const playlistAddNewAlbumFunction  = (currentSong) => {
-        let editedSong = { ...currentSong };
-        editedSong.albumcover = albumImage;
-        editedSong.albumName = albumName;
-        editedSong.albumId = albumId
-        addToCurrentSong(editedSong);
-        console.log(editedSong)
-      };
-  
-    const fetchAlbumHome = async () => {
-      try {
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const playlistAddNewAlbumFunction = (currentSong) => {
+    let editedSong = { ...currentSong };
+    editedSong.albumcover = albumImage;
+    editedSong.albumName = albumName;
+    editedSong.albumId = albumId;
+    addToCurrentSong(editedSong);
+    console.log(editedSong);
+  };
+
+  const fetchAlbumHome = async () => {
+    try {
       let response = await fetch(
         `https://striveschool-api.herokuapp.com/api/deezer/album/264645602`
       );
-    
-          let trackList = await response.json();
-          console.log(trackList.tracks);
-          setTrackArray(trackList.tracks.data);
-          setAlbumName(trackList.title);
-          setAlbumImage(trackList.cover_medium);
-          console.log(trackArray)
 
-        } catch (error) {
-            console.log(error);
-          }
-        };
+      let trackList = await response.json();
+      console.log(trackList.tracks);
+      setTrackArray(trackList.tracks.data);
+      setAlbumName(trackList.title);
+      setAlbumImage(trackList.cover_medium);
+      console.log(trackArray);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-        useEffect(() => {
-            fetchAlbumHome();
-          }, []);
-   
+  useEffect(() => {
+    fetchAlbumHome();
+  }, []);
 
-const _  = require("lodash"); 
+  const _ = require("lodash");
 
-// Original array 
-var array = trackArray 
+  // Original array
+  var array = trackArray;
 
-// Use of .shuffle() method
-let shuffled_array = _.shuffle(array);
+  // Use of .shuffle() method
+  let shuffled_array = _.shuffle(array);
 
-console.log(shuffled_array)
-
-
+  console.log(shuffled_array);
 
   return (
     <div>
-        <MDBRow>
-
+      <MDBRow>
         {shuffled_array.slice(0, 1).map((track, i) => (
           <MDBCard className="topbreathecard mb-5 ml-4" key={i}>
-              <div className="text-white d-flex align-items-center py-5 px-4">
-                <div id="sleepcardtitletop">
-                  <h3 className="card-title px-3" id="topcardhead"
-                  style={{ cursor: "pointer" }} onClick={() => playlistAddNewAlbumFunction(track)} >
-                  
-                    Listen to the track of the day now<MDBIcon icon="angle-right" id="topfavicon" />
-                    </h3>
-                     <div className="header__search mt-2">{/*<Button onClick={handleOpen} variant="outline-dark"> <PlayMod open={open} handleOpen={handleOpen} handleClose={handleClose} />{/*</Button>*/}</div>
-                  
+            <div className="text-white d-flex align-items-center py-5 px-4">
+              <div id="sleepcardtitletop">
+                <h3
+                  className="card-title px-3"
+                  id="topcardhead"
+                  style={{ cursor: "pointer" }}
+                  onClick={() => playlistAddNewAlbumFunction(track)}
+                >
+                  Listen to the track of the day now
+                  <MDBIcon icon="angle-right" id="topfavicon" />
+                </h3>
+                <div className="header__search mt-2">
+                  {/*<Button onClick={handleOpen} variant="outline-dark"> <PlayMod open={open} handleOpen={handleOpen} handleClose={handleClose} />{/*</Button>*/}
                 </div>
               </div>
+            </div>
           </MDBCard>
         ))}
-        </MDBRow>
-        </div>
-
-);
+      </MDBRow>
+    </div>
+  );
 };
 
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )(TopCardBreathe);
- 
+export default connect(mapStateToProps, mapDispatchToProps)(TopCardBreathe);
